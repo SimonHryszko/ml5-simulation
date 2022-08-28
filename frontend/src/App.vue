71ae0b48bd4b50a89961a074da0f7ca0091551d3
Simon Hryszko <simon@hryszko.dev>
2022-08-28 23:07:24 +0200

    Add corner preview label

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+    const isPreview = import.meta.env.VITE_PREVIEW;
+</script>
 
 <template>
     <main class="flex flex-col items-center mt-10">
+        <div
+            v-if="isPreview"
+            class="corner w-0 h-0 right-0 top-0 absolute border-l-[150px] border-l-transparent border-solid border-t-[150px] border-t-cyan-700">
+            <span
+                class="absolute block -top-28 -left-24 text-center rotate-45 w-16 text-xl font-mono"
+                title="This is pull request preview!">
+                Preview</span
+            >
+        </div>
+
         <!-- MENU -->
         <ul class="flex border-b">
             <li v-for="route in $router.options.routes" :key="route.path" class="-mb-px mr-1">
