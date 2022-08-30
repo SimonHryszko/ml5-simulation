91bf78962e454df601b4a93384bdd15602958f23
Simon Hryszko <simon@hryszko.dev>
2022-08-30 13:08:00 +0200

    Install sweetalert

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from './router/index';
 import './index.css';
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 const pinia = createPinia()
 const app = createApp(App)
