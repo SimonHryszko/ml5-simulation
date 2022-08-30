0cc7f03725c736d564d4ad5da4196518c3ad586b
Simon Hryszko <simon@hryszko.dev>
2022-08-30 13:23:46 +0200

    Made loading function

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,35 @@
 import { defineStore } from 'pinia';
+import * as modal from 'sweetalert2';
 
 export const data = defineStore('data', {
     state: () => {
-        return {
-        };
+        return {};
     },
+    actions: {
+        load() {
+            // Show info about loading
+            modal.fire({
+                title: 'Loading',
+                text: 'App is loading please wait!',
+                icon: 'info',
+                allowOutsideClick: false,
+                showConfirmButton: false,
+            });
 
+            // loading data
+
+            modal.close();
+            modal.fire({
+                toast: true,
+                title: 'Success',
+                text: 'Data loaded successfully',
+                icon: 'success',
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+            });
+        },
+    },
     persist: true,
 });
