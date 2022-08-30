ec59b90e7c05c6364aa2d3a9760d6536b9333088
Simon Hryszko <simon@hryszko.dev>
2022-08-30 13:23:46 +0200

    Made loading function

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,41 @@
 import { defineStore } from 'pinia';
+import * as modal from 'sweetalert2';
+
+const Toast = modal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', modal.stopTimer);
+        toast.addEventListener('mouseleave', modal.resumeTimer);
+    },
+});
 
 export const data = defineStore('data', {
     state: () => {
-        return {
-        };
+        return {};
     },
+    actions: {
+        load() {
+            // Show info about loading
+            modal.fire({
+                title: 'Loading',
+                text: 'App is loading please wait!',
+                icon: 'info',
+                allowOutsideClick: false,
+                showConfirmButton: false,
+            });
 
+            // loading data
+
+            modal.close();
+            Toast.fire({
+                icon: 'success',
+                title: 'App loaded!',
+            });
+        },
+    },
     persist: true,
 });
