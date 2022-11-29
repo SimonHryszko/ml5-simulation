7865e139e53121c97a76d22e1066bb32caa176f4
Simon Hryszko <szymon.hryszko@fitech.pl>
2022-11-29 16:35:50 +0100

    Clean project
    - Router file change
    - Fix top navigation menu condition
    - Remove unused model files by now
    - Start working on parameters component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router/index';
+import router from './router.js';
 import './index.css';
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
