ba851c16efb0713d4156c8c67668acb6c512262b
Simon Hryszko <szymon.hryszko@fitech.pl>
2022-12-13 18:14:43 +0100

    improve button when disabled

--- frontend/src/components/BaseButton.vue
+++ frontend/src/components/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="text-white font-medium bg-primary-red px-2 py-1.5 rounded-md" > 
+    <button class="text-white font-medium bg-primary-red px-2 py-1.5 rounded-md disabled:opacity-40" > 
         <slot></slot>
     </button>
 </template>
