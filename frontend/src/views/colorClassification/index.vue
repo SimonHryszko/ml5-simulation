348a5a0bba587bd416729dca6622354ead12b666
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "Added chart"
    This reverts commit a639e37c67af7aa43b744c4f00284d2f07992788.

--- frontend/src/views/colorClassification/index.vue
+++ frontend/src/views/colorClassification/index.vue
@@ -1,40 +1,6 @@
 <script setup>
     import { load, classify, result } from './model';
-    import { getColor } from '../../helper';
     import { ref, computed } from 'vue';
-    import { Bar } from 'vue-chartjs';
-    import {
-        Chart as ChartJS,
-        Title,
-        Tooltip,
-        Legend,
-        BarElement,
-        CategoryScale,
-        LinearScale,
-    } from 'chart.js';
-
-    ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale);
-    const data = computed(() => {
-        let labels = [];
-        let datasets = [
-            {
-                data: [],
-                backgroundColor: [],
-            },
-        ];
-        let backgroundColor = [];
-
-        result.value.map((item) => {
-            labels.push(item.label);
-            datasets[0].data.push((item.confidence * 100).toFixed(2));
-            datasets[0].backgroundColor.push(getColor(item.label));
-        });
-
-        return {
-            labels,
-            datasets,
-        };
-    });
 
     load();
     const colorChanged = (e) => {
@@ -49,11 +15,7 @@
 </script>
 <template>
     <div class="my-5">
-        <div class="flex space-x-2 items-center justify-center mb-5">
-            <p>Choose your color</p>
-            <input type="color" name="color" value="#e66465" @change="colorChanged" />
-        </div>
-        <Bar v-if="result.length > 0" :chart-data="data" />
-        <p v-else class="text-2xl font-mono font-semibold italic">Choose color to see chart!</p>
+        <input type="color" name="color" value="#e66465" @change="colorChanged" />
+        <p v-for="r in result" :key="r.label">{{r.label}}: {{r.confidence}}</p>
     </div>
 </template>
