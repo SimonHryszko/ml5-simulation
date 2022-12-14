bcb5cc8549dce78d454e8665ed448317357f74b5
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "Disable change color button if model not ready"
    This reverts commit 844a9c04b619142a19bcbac12a9ae60d2aba3bdb.

--- frontend/src/views/ColorClassification.vue
+++ frontend/src/views/ColorClassification.vue
@@ -120,10 +120,7 @@
         <section
             class="bg-black w-full rounded-md p-4 xl:col-start-11 flex flex-row gap-4 items-center justify-center xl:col-span-2 h-16">
             <div class="grid grid-cols-1 grid-rows-1 items-end">
-                <BaseButton
-                    :disabled="!modelReady"
-                    @click="selectColor"
-                    class="col-start-1 row-start-1 w-full"
+                <BaseButton @click="selectColor" class="col-start-1 row-start-1 w-full"
                     >Change Color</BaseButton
                 >
                 <input
