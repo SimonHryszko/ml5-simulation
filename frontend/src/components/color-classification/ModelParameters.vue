0ca01f837638830705f16824a7646a8c9bc4b331
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "Move model logic"
    This reverts commit 36518993d3fba6195de0339ca709662e25edb584.

--- frontend/src/components/color-classification/ModelParameters.vue
+++ frontend/src/components/color-classification/ModelParameters.vue
@@ -2,50 +2,15 @@
     import ChangeValueModule from './ChangeValueModule.vue';
     import { ref } from 'vue';
     import BaseButton from '@/components/BaseButton.vue';
-    import data from '@/Models/color-classification/data.json';
-    import ml5 from 'ml5';
 
     const emit = defineEmits(['update:modelValue']);
+
     const props = defineProps({
         modelValue: {
             required: true,
             type: Object,
         },
     });
-    const nnOptions = {
-        task: 'classification',
-        debug: false,
-    };
-
-    // ---- Callbacks ----
-    function finishedTraining() {
-        console.log('finished training');
-    }
-
-    // ---- Methods ----
-    const prepareModel = () => {
-        let nn = ml5.neuralNetwork(nnOptions);
-
-        data.forEach((item) => {
-            nn.addData(
-                {
-                    r: item.r,
-                    g: item.g,
-                    b: item.b,
-                },
-                {
-                    color: item.color,
-                },
-            );
-        });
-
-        nn.normalizeData();
-        nn.train(props.modelValue, finishedTraining);
-    };
-
-    const reteachModel = () => {
-        prepareModel();
-    };
 
     const test = ref(3);
 </script>
@@ -54,11 +19,15 @@
     <section class="flex flex-col gap-4 bg-black rounded-md p-4 w-full h-full z-10">
         <p>Parameters</p>
 
-        <ChangeValueModule titleName="Epochs" v-model="modelValue.epochs" />
-        <ChangeValueModule titleName="Batch size" v-model="modelValue.batchSize" />
+        <ChangeValueModule
+            titleName="Epochs"
+            v-model="modelValue.epochs" />
+        <ChangeValueModule
+            titleName="Batch size"
+            v-model="modelValue.batchSize" />
 
-        <BaseButton @click="reteachModel" class="col-start-1 row-start-1 w-full">
-            Reteach model
-        </BaseButton>
+        <BaseButton @click="selectColor" class="col-start-1 row-start-1 w-full"
+            >Reteach model</BaseButton
+        >
     </section>
 </template>
