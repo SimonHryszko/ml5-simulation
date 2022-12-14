8da32fd6b8becc99b0ef58cd72098afa755343d1
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "Improve component change value module"
    This reverts commit 9260e4207b76ca27cf25f18e8b42f69043a4436d.

--- frontend/src/components/color-classification/ChangeValueModule.vue
+++ frontend/src/components/color-classification/ChangeValueModule.vue
@@ -1,5 +1,4 @@
 <script setup>
-    import { ref } from 'vue';
     const props = defineProps({
         modelValue: {
             required: true,
@@ -9,13 +8,7 @@
             required: true,
             type: String,
         },
-        values: {
-            required: false,
-            type: Array,
-        },
     });
-
-    const values = ref(props.values ?? [1, 5, 10]);
     const emit = defineEmits(['update:modelValue']);
     const updateModelValue = (value) => {
         emit('update:modelValue', props.modelValue + value);
@@ -25,24 +18,30 @@
 <template>
     <section class="flex gap-4 justify-center items-center">
         <div
-            v-for="value in values.sort((a, b) => b - a)"
-            @click="updateModelValue(value * -1)"
+            @click="updateModelValue(-5)"
             class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
-            {{ value * -1 }}
+            -5
         </div>
-
+        <div
+            @click="updateModelValue(-1)"
+            class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
+            -1
+        </div>
         <div class="flex flex-col items-center w-2/12">
             <p class="text-xs text-gray-400">{{ props.titleName }}</p>
             <p class="" @change="emit('update:modelValue', $event.target.value)">
                 {{ modelValue }}
             </p>
         </div>
-
         <div
-            v-for="value in values.sort((b, a) => b - a)"
-            @click="updateModelValue(value)"
+            @click="updateModelValue(1)"
             class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
-            +{{ value }}
+            +1
+        </div>
+        <div
+            @click="updateModelValue(5)"
+            class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
+            +5
         </div>
     </section>
 </template>
