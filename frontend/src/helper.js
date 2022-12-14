977f7953d50c8a6c9561733a1f72c3da5fd5f17a
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "Make helper function ger color"
    This reverts commit 65423b8081d833a7ed8a29778c54644ce9473922.

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,5 +1,4 @@
 import * as modal from 'sweetalert2';
-import data from './views/colorClassification/data.json';
 
 export const Toast = modal.mixin({
     toast: true,
@@ -12,17 +11,3 @@ export const Toast = modal.mixin({
         toast.addEventListener('mouseleave', modal.resumeTimer);
     },
 });
-
-export const getColor = (name, type = 'rgba') => {
-    let color = data.filter((item) => item.color === name);
-
-    if (color.length > 0) {
-        color = color[0];
-    } else {
-        color = { r: 0, g: 0, b: 0 };
-    }
-
-    if (type == 'rgba') {
-        return `rgba(${color.r}, ${color.g}, ${color.b}, 1)`;
-    }
-};
