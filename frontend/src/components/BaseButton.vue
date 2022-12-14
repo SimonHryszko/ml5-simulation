af26955c897e49024a6938484bf3e4867bebe124
Simon Hryszko <s37198@s.pwste.edu.pl>
2022-12-15 00:59:28 +0100

    Revert "improve button when disabled"
    This reverts commit ba851c16efb0713d4156c8c67668acb6c512262b.

--- frontend/src/components/BaseButton.vue
+++ frontend/src/components/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="text-white font-medium bg-primary-red px-2 py-1.5 rounded-md disabled:opacity-40" > 
+    <button class="text-white font-medium bg-primary-red px-2 py-1.5 rounded-md" > 
         <slot></slot>
     </button>
 </template>
