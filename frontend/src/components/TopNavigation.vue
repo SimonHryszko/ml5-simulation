232ca72e3dbbd3a1b4f5033847baa77df25ba291
Simon Hryszko <szymon.hryszko@fitech.pl>
2022-12-15 01:32:48 +0100

    Revert "Add animation to logo"
    This reverts commits: 553e929eb82218fe545e9a1e34731431ddad5ce3, 96bc951e542a2a62398870d998e76ebbf66687fb.

--- frontend/src/components/TopNavigation.vue
+++ frontend/src/components/TopNavigation.vue
@@ -2,34 +2,6 @@
     import { useRoute } from 'vue-router';
     import BaseButton from '@/components/BaseButton.vue';
     import { computed } from 'vue';
-    import anime from 'animejs';
-    import { onMounted, nextTick } from 'vue';
-
-    onMounted(() => {
-        anime({
-            targets: '.anime-letter',
-            translateY: [-10, 0],
-            delay: anime.stagger(50),
-            opacity: [0, 1],
-            easing: 'easeOutExpo',
-            duration: 5000,
-            color: [...colors,...colors,...colors.reverse()],
-            changeComplete: () => {
-                nextTick(() => {
-                    anime({
-                    targets: '.anime-letter',
-                    color: ['#fff'],
-                    delay: anime.stagger(50),
-                        easing: 'easeOutExpo',
-                        duration: 500,
-                });
-                });
-            },
-        });
-    });
-
-const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8B00FF'
-];
 
     // get route path
     const route = useRoute();
@@ -41,15 +13,10 @@ const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082'
 </script>
 
 <template>
-    <nav
-        class="w-full items-center flex hover:cursor-pointer flex-row justify-between p-4 bg-black xl:px-16">
+    <nav class="w-full items-center flex hover:cursor-pointer flex-row justify-between p-4 bg-black xl:px-16">
         <div @click="$router.push('/')" class="flex flex-col gap-1">
             <h1 class="text-2xl font-semibold">ML - 5</h1>
-            <p class="opacity-70 text-xs flex">
-                <p class="anime-letter whitespace-pre" v-for="i in 'By Simon Hryszko'" :key="i">
-                    {{ i }}
-                </p>
-            </p>
+            <p class="opacity-70 text-xs">By Simon Hryszko</p>
         </div>
 
         <div v-if="!isLandingPage" class="flex flex-row gap-4">
