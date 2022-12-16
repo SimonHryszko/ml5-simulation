b759e37533ca4bc577273ed9558ad645050e2099
Simon Hryszko <szymon.hryszko@fitech.pl>
2022-12-16 16:09:04 +0100

    Fix render problem

--- frontend/src/components/color-classification/ChangeValueModule.vue
+++ frontend/src/components/color-classification/ChangeValueModule.vue
@@ -1,5 +1,7 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
+    import { orderBy, _ } from 'lodash';
+
     const props = defineProps({
         modelValue: {
             required: true,
@@ -15,7 +17,8 @@
         },
     });
 
-    const values = ref(props.values ?? [1, 5, 10]);
+    // computed with parameter
+    const values = ref(_.orderBy(props.values ?? [1, 5, 10]));
     const emit = defineEmits(['update:modelValue']);
     const updateModelValue = (value) => {
         emit('update:modelValue', props.modelValue + value);
@@ -24,23 +27,26 @@
 
 <template>
     <section class="flex gap-4 justify-center items-center">
+
         <div
-            v-for="value in values.sort((a, b) => b - a)"
+            v-for="value in orderBy(props.values ?? [1, 5, 10], Array, 'desc')"
             @click="updateModelValue(value * -1)"
+            :key="value + 'negative' + props.titleName"
             class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
-            {{ value * -1 }}
+            -{{ value }}
         </div>
 
         <div class="flex flex-col items-center w-2/12">
             <p class="text-xs text-gray-400">{{ props.titleName }}</p>
-            <p class="" @change="emit('update:modelValue', $event.target.value)">
+            <p>
                 {{ modelValue }}
             </p>
         </div>
 
         <div
-            v-for="value in values.sort((b, a) => b - a)"
+            v-for="value in orderBy(props.values ?? [1, 5, 10], Array, 'asc')"
             @click="updateModelValue(value)"
+            :key="value + 'positive' + props.titleName"
             class="cursor-pointer aspect-square w-10 text-center bg-primary-red text-black p-2 rounded-sm">
             +{{ value }}
         </div>
