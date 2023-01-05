8a67dea5d933708a11c58b9bb974d28158d78850
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-05 09:17:48 +0100

    Initial functionality
    - layout
    - temp classification for debug purpose
    - json anser show

---
+++ frontend/src/components/CodeAndSchema.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const props = defineProps({
+    code: {
+      type: String,
+      required: true,
+    },
+    schema: {
+      type: String,
+      required: true,
+    },
+  });
+
+</script>
+
+<template>
+  <section class="flex flex-col gap-4 bg-black rounded-md p-4 w-full h-full z-10">
+    <code>
+      JSON: {{ props.code }}
+    </code>
+  </section>
+</template>
