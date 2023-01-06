537dd036db462b7544c4f5a5e94a7fde921f48e6
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-06 19:11:12 +0100

    update code and schema component

--- frontend/src/components/CodeAndSchema.vue
+++ frontend/src/components/CodeAndSchema.vue
@@ -1,6 +1,8 @@
 <script setup>
-  import JsonViewer from 'vue-json-viewer'
-  import 'vue-json-viewer/style.css'
+  import JsonViewer from 'vue-json-viewer';
+  import 'vue-json-viewer/style.css';
+  import { ref, computed } from 'vue';
+  import Tabs from '../components/Tabs.vue';
 
   const props = defineProps({
     code: {
@@ -12,14 +14,18 @@
       required: true,
     },
   });
+  const selectedTab = ref('Code');
+  const tabOptions = ['Code', 'Schema'];
+  const jvCode = computed(() => {
+    return props[selectedTab.value.toLowerCase()];
+  });
 </script>
 
 <template>
   <section class="flex flex-col gap-4 bg-black rounded-md p-4 w-full h-full z-10">
+    <Tabs v-model="selectedTab" :options="tabOptions" />
     <code>
-      <json-viewer :value="props.code"
-      theme="jv-dark"
-      ></json-viewer>
+      <json-viewer :value="jvCode" theme="jv-dark"></json-viewer>
     </code>
   </section>
 </template>
