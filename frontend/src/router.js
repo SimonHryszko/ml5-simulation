05757d52fbbaa692078f22ad200624e5ac05c4c8
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-07 19:12:07 +0100

    new view for text classification

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,26 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    {
-        name: 'Landing page',
-        path: '/',
-        component: () => import('./views/LandingView.vue'),
-    },
-    {
-        name: 'Color classification',
-        path: '/color-classification',
-        component: () => import('./views/ColorClassification.vue'),
-    },
-    {
-        name: 'Image classification',
-        path: '/image-classification',
-        component: () => import('./views/ImageClassification.vue'),
-    },
+  {
+    name: 'Landing page',
+    path: '/',
+    component: () => import('./views/LandingView.vue'),
+  },
+  {
+    name: 'Color classification',
+    path: '/color-classification',
+    component: () => import('./views/ColorClassification.vue'),
+  },
+  {
+    name: 'Image classification',
+    path: '/image-classification',
+    component: () => import('./views/ImageClassification.vue'),
+  },
+  {
+    name: 'Text classification',
+    path: '/text-classification',
+    component: () => import('./views/TextClassification.vue'),
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
