66f2fc77454e7f7285300985e95a56a7cd4e0ffa
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-13 16:39:42 +0100

    remove button clasify

--- frontend/src/views/TextClassification.vue
+++ frontend/src/views/TextClassification.vue
@@ -106,12 +106,6 @@
         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Write your thoughts here..."></textarea>
 
-      <button
-        @click="classify(text)"
-        class="w-10/12 bg-transparent self-center hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Check review!
-      </button>
-
       <div class="flex-col gap-4 flex justify-center items-center">
         <div class="flex" v-show="result?.score">
           <span
