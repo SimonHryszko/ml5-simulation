b38298456267d0a43b2eb61c959eb515c92a325b
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-13 16:27:36 +0100

    Make copyToClipboard function

--- frontend/src/views/TextClassification.vue
+++ frontend/src/views/TextClassification.vue
@@ -64,6 +64,15 @@
       stars: 6,
     },
   ];
+  const copyToClipboard = (text) => {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
   onMounted(() => {
     // init model
     sentiment = ml5.sentiment('movieReviews', () => {
