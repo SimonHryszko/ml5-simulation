11463067eba4c98c2648e674dd18ff2c6097eeef
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-13 18:41:26 +0100

    Remove event pointer from preview label (PR only)

--- frontend/src/components/Preview.vue
+++ frontend/src/components/Preview.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="corner w-0 h-0 right-0 top-0 absolute border-l-[150px] border-l-transparent border-solid border-t-[150px] border-t-cyan-700">
+        class="pointer-events-none corner w-0 h-0 right-0 top-0 absolute border-l-[150px] border-l-transparent border-solid border-t-[150px] border-t-cyan-700">
         <span
             class="absolute block -top-28 -left-24 text-center rotate-45 w-16 text-xl font-mono"
             title="This is pull request preview!">
