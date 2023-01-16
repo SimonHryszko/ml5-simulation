11869d1a507a9055e0e7ae48c3805581931ab939
Simon Hryszko <szymon.hryszko@fitech.pl>
2023-01-16 19:09:54 +0100

    write test for hsl to hex func

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -32,4 +32,17 @@ export const hexToRgb = (hex) => {
 
 export const randomHexColor = () => {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+};
+
+export const hslToHex = (h, s, l) => {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, '0'); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+};
