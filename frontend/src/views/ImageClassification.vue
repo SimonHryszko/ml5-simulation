328b13cea883b3776a3c22a3fd720dd4d6391a12
Simon Hryszko <simno@hryszko.dev>
2023-01-18 21:11:16 +0100

    add modal to ic

--- frontend/src/views/ImageClassification.vue
+++ frontend/src/views/ImageClassification.vue
@@ -2,8 +2,9 @@
   import { ref, watch } from 'vue';
   import CodeAndSchemaVue from '../components/CodeAndSchema.vue';
   import ml5 from 'ml5';
+  import BaseButton from '@/components/BaseButton.vue';
   import ImageSwitcher from '../components/image-classification/ImageSwitcher.vue';
-  import { randomHexColor } from '@/helper.js';
+import { randomHexColor, guideModal } from '@/helper.js';
 
   const color = ref(randomHexColor());
   const img_name = ref('/image-classification/img1.png');
@@ -23,6 +24,48 @@
     classify();
   });
   const model = [{ label: 'String', confidence: 'Number' }];
+  const messages = [
+    {
+      title: 'Image Classification',
+      text: 'Image classification is a computer vision technique that allows us to classify an image into a set of predefined classes. In this example, we are using the MobileNet model to classify an image into 1000 classes.',
+    },
+    {
+      title: "Classed",
+      html: "The model can classify images into 1000 classes. Some of the classes are: 'cock', 'dining table, board', 'barber chair', 'padlocks', or 'tick'. <a href='https://deeplearning.cms.waikato.ac.nz/user-guide/class-maps/IMAGENET/' class='underline' target='_blank'>See all classes!</a>"
+    },
+    {
+      title: 'MobileNet',
+      text: 'MobileNet is a convolutional neural network that is 14x smaller and 125x faster than the original model, yet still produces higher accuracy. It is designed to work on mobile and embedded devices.',
+    },
+    {
+      title: 'Try it yourself!',
+      text: 'You can try it yourself by uploading your own image or by using one of the images provide'
+    },
+    {
+      title: 'Rememeber',
+      html: "Remember to use classes from the <a href='https://deeplearning.cms.waikato.ac.nz/user-guide/class-maps/IMAGENET/' class='underline' target='_blank'>list!</a>",
+      icon: 'info'
+    },
+    {
+      title: 'Results',
+      text: 'On the left will be shown 3 most relevant classes!'
+    },
+    {
+      title: 'Code',
+      text: 'And on top left corner you can see the code and schema of the model as you need!'
+    },
+    {
+      title: 'Enjoy!',
+      text: 'Enjoy the power of AI!',
+      color: '#716add',
+      backdrop: `
+        rgba(0,0,123,0.4)
+        url("/images/nyan-cat.gif")
+        left top
+        no-repeat
+      `
+    }
+  ]
 </script>
 <template>
   <section
@@ -53,5 +96,6 @@
     </div>
 
     <ImageSwitcher v-model="img_name" />
+    <BaseButton @click="guideModal(messages, 0)" class="bottom-5 absolute left-5">Guide</BaseButton>
   </section>
 </template>
