3e8ce4e6e6fe434a8d8934813eb4ef2112bccece
Simon Hryszko <simno@hryszko.dev>
2023-01-19 18:00:12 +0100

    Add new landing page item
    - new color
    - new landing page item

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                primary: {
-                    red: '#FF5353',
-                    blue: '#5364FF',
-                    yellow: '#FFA653',
-                },
-
-                black: {
-                    dark: '#161616',
-                    DEFAULT: '#1A1A1A',
-                },
-            },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          red: '#FF5353',
+          blue: '#5364FF',
+          yellow: '#FFA653',
+          pink: '#ff53e2',
         },
+
+        black: {
+          dark: '#161616',
+          DEFAULT: '#1A1A1A',
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
